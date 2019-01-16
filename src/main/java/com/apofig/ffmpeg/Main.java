92404917b92f09c698b5eb16b844514695e8ad43
alexander.baglay <apofig@gmail.com>
2019-01-16 10:13:17 +0200

    Рефакторинг, готовлю запуск других команд

--- src/main/java/com/apofig/ffmpeg/Main.java
+++ src/main/java/com/apofig/ffmpeg/Main.java
@@ -3,20 +3,26 @@ package com.apofig.ffmpeg;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Process p = Runtime.getRuntime().exec(
-                "ffmpeg -version"
-        );
+        List<String> info = exec("ffmpeg -version");
+        info.forEach(System.out::println);
+    }
 
-        try (BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+    private static List<String> exec(String command) throws IOException {
+        Process process = Runtime.getRuntime().exec(command);
+
+        List<String> messages = new LinkedList<>();
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
             String line;
-
             while ((line = input.readLine()) != null) {
-                System.out.println(line);
+                messages.add(line);
             }
         }
+        return messages;
     }
 }
